88e7f78806979e24adb6015a265c1dcf01009e3d
Perry <tavorp12@gmail.com>
2017-12-18 19:00:28 +0200

    Mass Update for current files - 18.12.17

---
+++ js/js_file.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    getAnimalTitles();
+    animalTitleTransition();
+
+});
+
+/**
+ * Get all titles from the animals page
+ */
+function getAnimalTitles() {    
+ var animals = $('.animal');
+
+    for (var i = 0; i < animals.length; i++) {
+        var animal = animals[i].id;
+        $('#' + animal + ' .description').load(animal + ".html .pmain h2").text();
+    }
+}
+
+/**
+ * Mouse in mouse out transition
+ */
+function animalTitleTransition() {
+
+    // Mouse over
+    var animalImage = $('.animal-image');
+    animalImage.on('mouseover', function() {
+        var image = $(this).find('img');
+        var description = $(this).find('.description');
+
+        image.css('opacity', 0.5);
+        description.css('display', 'block');
+    });
+
+    // Mouse out
+    animalImage.on('mouseout', function() {
+        var image = $(this).find('img');
+        var description = $(this).find('.description');
+
+        image.css('opacity', 1);
+        description.css('display', 'none');
+    });
+}
