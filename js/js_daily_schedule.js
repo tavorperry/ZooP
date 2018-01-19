4cfbc3e9ff263b292114f5b27ed0c2f8a315322f
Tavor <tavorp12@gmail.com>
2018-01-19 23:38:06 +0200

    JS Added to Page !

---
+++ js/js_daily_schedule.js
@@ -0,0 +1,16 @@
+ var d = new Date();
+ var n = d.getDay();
+  if(n==0)
+document.write("<style> #sun{background-color: yellow; color: black;} </style>");
+else if(n==1)
+document.write("<style> #mon{background-color: yellow; color: black;} </style>");
+else if(n==2)
+document.write("<style> #tuw{background-color: yellow; color: black;} </style>");
+else if(n==3)
+document.write("<style> #wed{background-color: yellow; color: black;} </style>");
+else if(n==4)
+document.write("<style> #thu{background-color: yellow; color: black;} </style>");
+else if(n==5)
+document.write("<style> #fri{background-color: yellow; color: black;} </style>");
+else if(n==6)
+document.write("<style> #sat{background-color: yellow; color: black;} </style>");
