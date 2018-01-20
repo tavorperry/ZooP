ae37db3ba9e713aaf98d5362fee47248abd290b5
cygelman <33072785+cygelman@users.noreply.github.com>
2018-01-20 15:55:06 +0200

    JS fix

--- js/js_thank_you.js
+++ js/js_thank_you.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-    if(typeof jQuery=="undefined"){
-        alert("jQuery id undefined");
-    }
+    setTimeout(function() {
+        window.location.href = "index2.html";
+    }, 12000);
 });
 
-setTimeout(function() {
-  window.location.href = "index2.html";
-}, 12000);
+
