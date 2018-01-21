b6d11899a8254513377af461f3f7a45fdb0b428f
Almog Cygel <cygelman@gmail.com>
2018-01-21 19:41:14 +0200

    Meta data addition + UTF-8

--- includes/thank_you.php
+++ includes/thank_you.php
@@ -2,12 +2,13 @@
 <html>
 <head>
     <title>Thank you</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../css/volunteer.css">
-    <link rel="stylesheet" type="text/css" href="../css/thank_you.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Acme|Bree+Serif|Indie+Flower|Josefin+Sans:600" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/volunteer.css">
+    <link rel="stylesheet" type="text/css" href="../css/thank_you.css">
   </head>
 
   <body>
